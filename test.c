3a8f029be361946b14b9c113084cadce59219372
praveen-ab <32394983+praveen-ab@users.noreply.github.com>
2019-05-22 11:21:31 +0530

    Update test.c

--- test.c
+++ test.c
@@ -1,25 +1,15 @@
 #include <stdio.h>
 int main()
 {
-      double firstNumber, secondNumber, temporaryVariable;
+    int number;
 
-      printf("Enter first number: ");
-      scanf("%lf", &firstNumber);
-
-      printf("Enter second number: ");
-      scanf("%lf",&secondNumber);
-
-      // Value of firstNumber is assigned to temporaryVariable
-      temporaryVariable = firstNumber;
-
-      // Value of secondNumber is assigned to firstNumber
-      firstNumber = secondNumber;
-
-      // Value of temporaryVariable (which contains the initial value of firstNumber) is assigned to secondNumber
-      secondNumber = temporaryVariable;
-
-      printf("\nAfter swapping, firstNumber = %.2lf\n", firstNumber);
-      printf("After swapping, secondNumber = %.2lf", secondNumber);
-
-      return 0;
+    // printf() dislpays the formatted output 
+    printf("Enter an integer: ");  
+    
+    // scanf() reads the formatted input and stores them
+    scanf("%d", &number);  
+    
+    // printf() displays the formatted output
+    printf("You entered: %d", number);
+    return 0;
 }
